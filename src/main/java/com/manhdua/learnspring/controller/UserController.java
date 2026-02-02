44ba0e5abda8d8c7f1484e01dc6f60c4651b86f0
manhdua1 <manhvu20012005@gmail.comgit config --global user.email manhvu20012005@gmail.com>
2026-02-02 10:17:19 +0700

    Add CRUD for user

--- src/main/java/com/manhdua/learnspring/controller/UserController.java
+++ src/main/java/com/manhdua/learnspring/controller/UserController.java
@@ -1,20 +1,43 @@
 package com.manhdua.learnspring.controller;
 
 import com.manhdua.learnspring.dto.request.UserCreationRequest;
+import com.manhdua.learnspring.dto.request.UserUpdateRequest;
 import com.manhdua.learnspring.entity.User;
 import com.manhdua.learnspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     User createUser(@RequestBody UserCreationRequest userCreationRequest) {
         return userService.createUser(userCreationRequest);
+    }
+
+    @GetMapping
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable UUID userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable UUID userId, @RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    void deleteUser(@PathVariable UUID userId) {
+        userService.deleteUser(userId);
     }
 }
--- src/main/java/com/manhdua/learnspring/service/UserService.java
+++ src/main/java/com/manhdua/learnspring/service/UserService.java
@@ -1,10 +1,14 @@
 package com.manhdua.learnspring.service;
 
 import com.manhdua.learnspring.dto.request.UserCreationRequest;
+import com.manhdua.learnspring.dto.request.UserUpdateRequest;
 import com.manhdua.learnspring.entity.User;
 import com.manhdua.learnspring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -22,5 +26,29 @@ public class UserService {
 
         userRepository.save(user);
         return user;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(UUID id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(UUID userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(UUID userId) {
+        userRepository.deleteById(userId);
     }
 }
